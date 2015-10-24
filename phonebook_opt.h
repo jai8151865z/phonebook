ec59796d94816eb29731540a901d4b3b59d0f47a
jai8151865z <jai8151865z@gmail.com>
2015-10-24 18:47:01 +0800

    phonebook_opt.h:change struct

--- phonebook_opt.h
+++ phonebook_opt.h
@@ -6,15 +6,7 @@
 /* original version */
 typedef struct __PHONE_BOOK_ENTRY {
     char lastName[MAX_LAST_NAME_SIZE];
-    char firstName[16];
-    char email[16];
-    char phone[10];
-    char cell[10];
-    char addr1[16];
-    char addr2[16];
-    char city[16];
-    char state[2];
-    char zip[5];
+    entry *detail;
     struct __PHONE_BOOK_ENTRY *pNext;
 } entry;
 
