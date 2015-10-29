bf5b1ce3c5734b796c65675d41e5e9d3f5da7d35
jai8151865z <jai8151865z@gmail.com>
2015-10-30 03:54:45 +0800

    cteate new file phonebook_hash.c

---
+++ phonebook_hash.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#include "phonebook_hash.h"
+
+entry* HashTable[Hash_Table_Size];
+
+/* hash version */
+
+static inline int hash(char lastname[])
+{
+     int letnumber;
+     int val=0;
+     for(int i=0;i<(sizeof(lastname)/sizeof(lastname[0]));i++)
+     {
+           letnumber = lastname[i];
+           val += letnumber;
+      }
+
+      return val%Hash_Table_Size;
+}
+
+entry *findName(char lastname[], entry *pHead)
+{
+    pHead = HashTable[hash(lastname)];
+
+    while (pHead != NULL) {
+        if (strcasecmp(lastname, pHead->lastName) == 0)
+            return pHead;
+        pHead = pHead->pNext;
+    }
+    return NULL;
+}
+
+entry *append(char lastName[], entry *e)
+{
+    int tablenumber = hash(lastName);
+    e->pNext = (entry *) malloc(sizeof(entry));
+    e = e->pNext;
+    if(HashTable[tablenumber] != NULL)
+    {
+          strcpy(e->lastName, lastName);
+          e->pNext = NULL;
+          HashTable[tablenumber]->pNext = e;
+     }
+     else
+     {
+          strcpy(e->lastName, lastName);
+          e->pNext = NULL;
+          HashTable[tablenumber] = e;
+      }
+
+    return e;
+}
